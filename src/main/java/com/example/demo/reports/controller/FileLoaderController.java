b8cf3e795d8999e8f225aaa8b8e6f5a70f5a3fb7
yekr0717 <!Kotik123>
2018-04-03 11:15:49 +0300

    both file uploading works

--- src/main/java/com/example/demo/reports/controller/FileLoaderController.java
+++ src/main/java/com/example/demo/reports/controller/FileLoaderController.java
@@ -8,20 +8,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.List;
 
 @Controller
 @Slf4j
 public class FileLoaderController {
 
     private final QueryExecutor queryExecutor;
-       @Autowired
+
+    @Autowired
     public FileLoaderController(QueryExecutor queryExecutor) {
-        this.queryExecutor = queryExecutor;}
+        this.queryExecutor = queryExecutor;
+    }
 
     @GetMapping(value = "/report/download/webContent")
     public String downloadContentInfo(Model model)
@@ -42,31 +46,35 @@ public class FileLoaderController {
     }
 
     @PostMapping(value = "/report/loadSingleFile")
-    public ModelAndView singleFileUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request){
+    public String singleFileUpload(HttpServletRequest request, RedirectAttributes redirectAttributes) throws Exception {
         log.info("singleFileUpload started");
-        ModelAndView modelAndView = new ModelAndView("report");
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        MultipartFile file = multipartRequest.getFile("multipartFile");
         if (file.isEmpty()) {
-            modelAndView.addObject("errorMessage", "Selected file is empty. Choose another one, please.");
-            return modelAndView;
+          redirectAttributes.addFlashAttribute("errorMessage", "File " + file.getOriginalFilename() + " is empty. Choose another one, please.");
+          return ("redirect:/report");
         }
-        FileHelper.upload(file,request, "xlsReports");
-        modelAndView.addObject("successMessage", "File " + file.getName() + "successfully uploaded");
-        return modelAndView;
+        File report = FileHelper.upload(file, request, "xlsReports");
+        redirectAttributes.addFlashAttribute("successMessage", "File " + file.getOriginalFilename() + " successfully uploaded");
+        redirectAttributes.addFlashAttribute("report", report);
+        return ("redirect:/report");
     }
 
     @PostMapping(value = "/report/loadMultiFiles")
-    public ModelAndView multiFileUpload(@RequestParam("files") MultipartFile[] files, HttpServletRequest request){
+    public String multiFileUpload(HttpServletRequest request, RedirectAttributes redirectAttributes) throws Exception {
         log.info("multiFileUpload started");
-        ModelAndView modelAndView = new ModelAndView("report");
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        List<MultipartFile> files = multipartRequest.getFiles("files");
         for (MultipartFile file : files) {
             if (file.isEmpty()) {
-                modelAndView.addObject("errorMessage", "File "+ file.getName()+ "is empty. Choose another one, please.");
-                return modelAndView;
+                redirectAttributes.addFlashAttribute("errorMessage", "File " + file.getOriginalFilename() + " is empty. Choose another one, please.");
+                return ("redirect:/report");
+            } else {
+                FileHelper.upload(file, request, "xlsReports");
             }
-            FileHelper.upload(file, request, "xlsReports");
-            modelAndView.addObject("successMessage", "File " + file.getName() + "successfully uploaded");
+            redirectAttributes.addFlashAttribute("successMessage", "Files successfully uploaded");
         }
-        return modelAndView;
+        return ("redirect:/report");
     }
 
 }
